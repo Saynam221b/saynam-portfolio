d5c2391498e089f655a38a820d6b827929a56feb
Saynam221b <saynam1101@gmail.com>
2025-01-20 01:01:03 +0530

    c

--- api/send-email.js
+++ api/send-email.js
@@ -9,14 +9,15 @@ export default async function handler(req, res) {
 
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
     tls: {
       rejectUnauthorized: false,
+      minVersion: 'TLSv1.2',
     },
   });
 
